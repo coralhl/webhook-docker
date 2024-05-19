fdc6abbcb015fb7e5b6d72a74ba41d8412456fb1
coral <coralhl@gmail.com>
2024-05-19 13:19:20 +0300

    up

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ docker buildx build --output type=docker --progress=plain \
   --build-arg="WEBHOOK_VERSION=$WEBHOOK_VERSION" \
   -f Dockerfile -t coralhl/webhook:latest -t coralhl/webhook:$WEBHOOK_VERSION .
 # Заливка в регистр
-#docker push coralhl/webhook:latest
-#docker push coralhl/webhook:$WEBHOOK_VERSION
+docker push coralhl/webhook:latest
+docker push coralhl/webhook:$WEBHOOK_VERSION
